a7c623b2584f35a84b6480d0660879ec6b5f48f6
Sandro Felicioni <sandro.felicioni@gmail.com>
2019-10-06 14:36:01 +0200

    Fix failing installation

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0.0',
     description='CRAFT module',
     packages=['text_detector_craft'],
-    install_requires=['torch==0.4.1.post2', 'torchvision==0.2.1', 'opencv-python==3.4.2.17', 'scikit-image==0.14.2', 'scipy==1.1.0'],
+    install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'opencv-python==3.4.2.17', 'scikit-image==0.14.2', 'scipy==1.1.0'],
 )
